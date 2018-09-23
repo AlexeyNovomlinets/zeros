4336d8cb211ce6cbdf4f23b582fac1ef2c8e055b
Aliaksei Navamlinets <alex.novomlinets@gmail.com>
2018-09-23 12:26:32 +0300

    feat: implement feature according to the task#7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var currentCount = 0;
+  var k = 1;
+  var res = 0;
+
+  function zerosCount(number) {
+    if ((number/(Math.pow(5, k))) >= 1) {
+      currentCount = Math.floor(number/(Math.pow(5, k)));
+      k++;
+      return (currentCount + zerosCount(number));
+    }
+    else return 0;
+  }
+  
+  return zerosCount(number);
 }
